864b7a6b4e51f67f30cdd8bcd7c516ebb18cb1f4
Abdullah Oğuz <oguzabdullah39@gmail.com>
2021-01-08 21:37:01 +0300

    no ack true changed

--- edducate-log-mongoDB/controller/rabbit_mq_consumer.js
+++ edducate-log-mongoDB/controller/rabbit_mq_consumer.js
@@ -3,20 +3,23 @@ import { mongoAdd } from "../service/db_service";
 
 export async function rabbitMQconsumer(channelName) {
   try {
-    //RabbitMQ Consumer s
+    //RabbitMQ Consumer
 
     const connection = amqp.connect("amqp://localhost:5672");
     const channel = (await connection).createChannel();
-     (await channel).assertQueue(channelName);
+    (await channel).assertQueue(channelName);
 
     console.log("Mesaj Bekleniyor...");
 
     (await channel).consume(channelName, (message) => {
       const messageInfo = JSON.parse(message.content.toString());
-
-      var parseData = JSON.parse(messageInfo);
+      
+      const parseData = JSON.parse(messageInfo);
+   
       //Write MongoDB
-      mongoAdd(parseData);
+       mongoAdd(parseData);
+    },{
+      noAck: true
     });
   } catch (error) {
     throw error;
