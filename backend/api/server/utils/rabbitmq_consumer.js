0c2fdeb3ae6590f4b04e463dad9ad93d21532d14
Abdullah Oğuz <oguzabdullah39@gmail.com>
2021-01-13 09:39:47 +0300

    delete connection

--- backend/api/server/utils/rabbitmq_consumer.js
+++ backend/api/server/utils/rabbitmq_consumer.js
@@ -3,7 +3,7 @@ import amqp, { connect } from 'amqplib'
  export async function connect_rabbitmq(logChannel, data){
   try {
     const connection = await amqp.connect('amqp://localhost:5672');
-    console.log('connection done' +connection);
+    
     const channel = await connection.createChannel();
     const assertion = await channel.assertQueue(logChannel);
 
